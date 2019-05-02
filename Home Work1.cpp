bc09a823b51c107d0c6ce01c55630b8b25e032f8
TsungKaiHo <50210177+TsungKaiHo@users.noreply.github.com>
2019-05-02 23:53:25 +0800

    Add files via upload

---
+++ Home Work1.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Home Work 1
+//
+//  Created by Ho Tsung Kai on 2019/3/15.
+//  Copyright © 2019 Ho Tsung Kai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    std::cout << "Hello World! 3A713213\n" ;
+    system("pause");
+    return 0;
+}
